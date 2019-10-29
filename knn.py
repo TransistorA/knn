8645586e4771846258277d7ef896e003892a5257
Annan Miao <am049@bucknell.edu>
2019-10-29 03:21:44 -0400

    add knn

--- knn.py
+++ knn.py
@@ -1,13 +1,43 @@
+import math
+
+
 class KNeighborsClassifier:
 
-    def __init(self, k):
+    def __init__(self, k):
         self.k = k
+        self.X_train = []
+        self.y_train = []
+
+    def euclidean_distance(self, row1, row2):
+        distance = 0
+        for i in range(len(row1)):
+            distance += (row1[i] - row2[i]) ** 2
+        return math.sqrt(distance)
 
     def fit(self, X_train, y_train):
-        raise NotImplementedError()
+        self.X_train = X_train
+        self.y_train = y_train
 
     def predict(self, X_test):
-        raise NotImplementedError()
+        predictions = []
 
-    def score(self, X_test, y_test):
-        raise NotImplementedError()
+        for data in X_test:
+            distances = []
+
+            for i in range(len(self.X_train)):
+                distance = self.euclidean_distance(data, self.X_train[i])
+                distances.append((distance, self.y_train[i]))
+
+            distances.sort(key=lambda x: x[0])
+
+            neighbors = []
+            for i in range(self.k):
+                neighbors.append(distances[i][1])
+
+            prediction = max(set(neighbors), key=neighbors.count)
+            predictions.append(prediction)
+
+        return predictions
+
+    def score(self, y_pred, y_test):
+        return (y_pred == y_test).mean()
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ X, y = utils.shuffle(X, y, random_state=1)
 train_set_size = 100
 X_train = X[:train_set_size]  # selects first 100 rows (examples) for train set
 y_train = y[:train_set_size]
-X_test = X[train_set_size:]   # selects from row 100 until the last one for test set
+X_test = X[train_set_size:]  # selects from row 100 until the last one for test set
 y_test = y[train_set_size:]
 
 k = 5
@@ -19,4 +19,4 @@ knn = KNeighborsClassifier(k=k)
 
 knn.fit(X_train, y_train)
 y_pred_test = knn.predict(X_test)
-print("Accuracy of KNN test set:", knn.score(X_test, y_test))
+print("Accuracy of KNN test set:", knn.score(y_pred_test, y_test))
